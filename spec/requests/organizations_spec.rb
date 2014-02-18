7dd7cb466443e9e5f6cfc94d1493a5d2f99b4670
johnnymo87 <johnnymo87@gmail.com>
2014-02-18 00:13:19 +0000

    monkey patched module method so that it follows redirects up to 6 times, then raises error

--- spec/requests/organizations_spec.rb
+++ spec/requests/organizations_spec.rb
@@ -3,9 +3,7 @@ require 'spec_helper'
 describe "Organizations" do
   describe "GET /organizations" do
     it "works! (now write some real specs)" do
-      user = FactoryGirl.create :user
-      # post_via_redirect
-      post user_session_path, 'user[email]' => user.email, 'user[password]' => user.password
+      get organizations_path
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,7 @@ Capybara.javascript_driver = :webkit
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir["./spec/support/**/*.rb"].sort.each {|f| require f}
 
 RSpec.configure do |config|
   # == Mock Framework
@@ -32,6 +32,7 @@ RSpec.configure do |config|
   config.include Devise::TestHelpers, :type => :helper
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include RequestSpecHelpers, :type => :request
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
