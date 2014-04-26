8bc91267cc1d205af2ea4709b46fa3b561a55bc6
johnnymo87 <johnnymo87@gmail.com>
2014-04-26 19:50:15 +0100

    not working like it used to

--- spec/requests/organizations_spec.rb
+++ spec/requests/organizations_spec.rb
@@ -1,9 +1,34 @@
 require 'spec_helper'
 
-describe "Organizations" do
-  describe "GET /organizations" do
-    it "works! (now write some real specs)" do
-      get organizations_path
-    end
+describe 'Organizations', :helpers => :requests do
+
+  # def check_routes(factory)
+  #   include ActionController::UrlFor
+  #
+  #   %w( index new create show edit update destroy )
+  #   object = FactoryGirl.create(factory)
+  #   param_key = object.class.to_s
+  #   controller_name = object.class.to_s.pluralize.underscore
+  #   attr = attributes_for(factory)
+  #   Rails.application.routes.routes.each do |route|
+  #     if route.defaults[:controller] == controller_name
+  #       route.defaults[:only_path] = true
+  #       debugger
+  #       url_for
+  #
+  #       path = url_for(route.defaults)
+  #       case :action
+  #         when 'index' || 'new' then eval("visit '#{path}'")
+  #         when 'create' || 'update' then eval("visit '#{path}', #{{param_key => attr}}")
+  #         when 'show' || 'edit' || 'destroy' then eval("visit '#{path}', #{{:id => object.id}}")
+  #         else next
+  #       end
+  #     end
+  #   end
+  #
+  # end
+
+  it '' do
+    check_routes :page
   end
 end
--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -19,4 +19,51 @@ module RequestHelpers
   def login(user)
     post_via_redirect user_session_path, 'user[email]' => user.email, 'user[password]' => user.password
   end
+
+
+  def check_routes(factory)
+    include ActionController::UrlFor
+
+    object = FactoryGirl.create(factory)
+    param_key = object.class.to_s
+    controller_name = object.class.to_s.pluralize.underscore
+    attr = attributes_for(factory)
+
+
+    Rails.application.routes.routes.each do |route|
+      if route.defaults[:controller] == controller_name
+
+        # url_for
+        route.defaults[:only_path] = true
+        path = url_for(route.defaults)
+
+        # verb = find_verb_from(route.verb)
+        # path = find_path_from(route.defaults)
+
+        puts 'hi'
+        debugger
+        puts 'lo'
+
+
+        case :action
+          when 'index' || 'new' then eval("visit '#{path}'")
+          when 'create' || 'update' then eval("visit '#{path}', #{{param_key => attr}}")
+          when 'show' || 'edit' || 'destroy' then eval("visit '#{path}', #{{:id => object.id}}")
+          else next
+        end
+      end
+    end
+  end
+
+  # private
+  #
+  # def find_verb_from(regex)
+  #   actions = %w(GET POST PUT DELETE)
+  #   actions.select{|a| a.match(regex) }.first.downcase
+  # end
+  #
+  # def find_path_from(action_param)
+  #   action_param[:only_path] = true
+  #   url_for action_param
+  # end
 end
