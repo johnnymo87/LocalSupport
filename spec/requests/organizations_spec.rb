8ad336041b44a2bbe324617f73238d88e4cfa3ae
johnnymo87 <johnnymo87@gmail.com>
2014-04-28 04:07:11 +0100

    a lot of small details required to run all actions

--- spec/requests/organizations_spec.rb
+++ spec/requests/organizations_spec.rb
@@ -1,9 +1,35 @@
 require 'spec_helper'
 
-describe "Organizations" do
-  describe "GET /organizations" do
-    it "works! (now write some real specs)" do
-      get organizations_path
+describe 'Organizations', :helpers => :requests do
+  extend RequestHelpers
+  instance = Organization.create!
+  accessible_attrs = FactoryGirl.attributes_for :organization
+
+  let(:non_admin) { FactoryGirl.create(:user) }
+  let(:admin) { FactoryGirl.create(:user, email: 'admin@example.com', admin: true) }
+
+  it 'test' do
+    puts 'hi'
+    debugger
+    puts 'lo'
+  end
+
+  describe 'admin can access all API actions' do
+    let(:org) { FactoryGirl.create(:organization) }
+    before { login(admin) }
+
+    for_actions_in(OrganizationsController, instance, accessible_attrs, :only => [:update]) do |action, command|
+      it "admin can access #{action}" do
+        # puts 'hi'
+        # debugger
+        # puts 'lo'
+
+        eval(command)
+        response.status.should be 200
+        flash[:error].should be nil
+        # request_via_redirect
+      end
     end
   end
+
 end
--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -19,4 +19,92 @@ module RequestHelpers
   def login(user)
     post_via_redirect user_session_path, 'user[email]' => user.email, 'user[password]' => user.password
   end
+
+  def for_actions_in(controller, instance, access_attrs, options = {})
+    tapper ||= RouteTapper.new(self, controller, instance, access_attrs, options)
+    # tapper.anonymize_controller
+    tapper.actions.each_pair do |action, command|
+      yield(action, command)
+      # eval("#{verb} :#{action}")
+      # yield
+    end
+  end
+
+  class RouteTapper
+    include Rails.application.routes.url_helpers
+
+    attr_reader :actions
+
+    def initialize(context, controller, instance, access_attrs, options = {})
+      @controller_name = controller.to_s.chomp('Controller').downcase
+
+      routes = Rails.application.routes.routes.select do |route|
+        route.defaults[:controller] == @controller_name
+      end
+
+      @instance = instance
+
+      @access_attrs = access_attrs
+
+      @context = context
+
+      @options = options
+
+      @actions = routes.each_with_object({}) do |route, hsh|
+        # puts 'hi'
+        # debugger
+        # puts 'lo'
+
+        @action = route.defaults[:action].to_sym
+        @verb = find_verb_from(route.verb)
+        @params = find_params_from(route.parts)
+
+        @url = find_url
+        hsh[@action] = make_executable if options_require @action
+      end
+    end
+
+    private
+
+    def make_executable
+      "request_via_redirect(:#{@verb}, '#{@url}', #{@params})"
+    end
+
+    def options_require(action)
+      if @options[:only]
+        @options[:only].include? action
+      elsif @options[:except]
+        @options[:except].exclude? action
+      else
+        true
+      end
+    end
+
+    def find_verb_from(regex)
+      actions = %w(GET POST PUT DELETE)
+      actions.select { |a| a.match(regex) }.first.downcase
+    end
+
+    def find_params_from(matcher_list)
+      matcher_list.reject! { |elem| elem == :format }
+      raise "can only handle [:id], but got #{matcher_list}" unless matcher_list.length <= 1
+      params = {}
+      params = {:id => @instance.to_param} if matcher_list.include?(:id)
+      params.merge! @access_attrs if upgrade_params?
+      params
+      puts 'hi'
+      debugger
+      puts 'lo'
+
+    end
+
+    def find_url
+      url_for only_path: true, controller: @controller_name, action: @action, id: @instance.to_param
+    end
+
+    def upgrade_params?
+      @action == :create || @action == :update
+    end
+  end
+
 end
