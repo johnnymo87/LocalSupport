a2d6a1f615005430f870fbcaf91e94dca1df458a
johnnymo87 <johnnymo87@gmail.com>
2014-05-20 20:42:55 +0100

    bad invitations in a pull request

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,12 +1,21 @@
 class InvitationsController < ApplicationController
   before_filter :authorize
 
-  # xhr only
+  # xhr only, tested in a request spec
   def create
-    render :json => ::Invitations.(
-      params,
-      current_user,
-    ).to_json
+    flag = params.fetch(:resend_invitation).to_s == 'true' ? true : false
+    Devise.resend_invitation = flag
+    answers = params.fetch(:invite_list).map do |invite|
+      user = User.invite!({email: invite.fetch(:email)}, current_user) do |user|
+        user.organization_id = invite.fetch(:id)
+      end
+      answer = if user.errors.any?
+                   user.errors.full_messages.map{|msg| "Error: #{msg}"}.join(' ')
+                 else
+                   'Invited!'
+                 end
+      {invite.fetch(:id) => answer}
+    end
+    render json: answers.reduce({}, :update).to_json
   end
-
 end
